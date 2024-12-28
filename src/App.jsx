a51d6258df39744b7658d392b0253bb31ae20c36
Oliver <oliver.stolpe@hotmail.se>
2024-12-28 23:11:41 +0100

    Added about me mage and updated README-file.

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,6 @@ const router = createBrowserRouter([
     element: <HomePage />,
   },
   {
-    path: "/projects",
-    element: <ProjectsPage />,
-  },
-  {
     path: "/about",
     element: <AboutPage />,
   },
